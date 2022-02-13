d4a01aff47080407e73120b807d0e85585e66b1c
unknown <muhamed_elsharqawy@yahoo.com>
2022-02-13 13:14:59 +0200

    second commit

--- main.js
+++ main.js
@@ -2,20 +2,26 @@ let inputEl = document.getElementById("input-el");
 let buttonEl = document.getElementById("button-el");
 let ulEl = document.getElementById("ul-el");
 let myArray = [];
+let oldArray = [];
+let deleteBtn = document.querySelector("#delete-el");
 
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myArray = [];
+  render(myArray);
+});
+
+function render(array) {
+  let x = "";
+  for (let i = 0; i < array.length; i++) {
+    x += "<li>" + "<a href='#'  target='_blank'>" + array[i] + "</a>" + "</li>";
+  }
+  ulEl.innerHTML = x;
+}
 buttonEl.addEventListener("click", function () {
   myArray.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myArray", JSON.stringify(myArray));
   console.log(localStorage.getItem("myArray"));
-  renderElement();
+  render(myArray);
 });
-
-function renderElement() {
-  let x = "";
-  for (let i = 0; i < myArray.length; i++) {
-    x +=
-      "<li>" + "<a href='#'  target='_blank'>" + myArray[i] + "</a>" + "</li>";
-  }
-  ulEl.innerHTML = x;
-}
